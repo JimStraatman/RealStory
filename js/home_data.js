d4e5caa2de4124867d71edb1ac6a46eb06032edd
Jim Straatman <jim.straatman@gmail.com>
2013-05-19 07:59:03 -0700

    Post updates

--- js/home_data.js
+++ js/home_data.js
@@ -24,22 +24,14 @@ function HomeListCtrl($scope) {
 				date: new Date(Date.parse('1/12/13'))
 			},
 			{
-
-				postBody: "Did you know Broadway Park is a historic district?",
-				postVideoURL: "",
-				postImageURl: "",
-				postLinkURL: "http://www.dahp.wa.gov/blog/2012/07/recent-state-and-national-register-listings/small/",
-				date: new Date(Date.parse('3/10/13'))
-			},
-			{
-				postBody: "There are 47 different tree varieties in Broadway Park, right ouside my doorstep.",
+				postBody: "47 tree varieties in Broadway Park, right ouside my doorstep!",
 				postVideoURL: "",
 				postImageURl: "http://www.flickr.com/photos/88279756@N07/8752387542/",
 				postLinkURL: "",
 				date: new Date(Date.parse('3/11/13'))
 			},
 			{
-				postBody: "Scott Rorvig, the previous owner, did an amazing expansion, keeping the style of the house intact.",
+				postBody: "Scott Rorvig, did an amazing expansion, keeping the style of the house intact.",
 				postVideoURL: "",
 				postImageURl: "http://www.flickr.com/photos/88279756@N07/8751275689/",
 				postLinkURL: "",
@@ -60,11 +52,18 @@ function HomeListCtrl($scope) {
 				date: new Date(Date.parse('4/12/13'))
 			},
 			{
-				postBody: "Just refinished the hardware floors, they look awesome!", 
+				postBody: "Robinson Hardwood refinished the floors, they look awesome!", 
 				postVideoURL: "",
 				postImageURl: "http://www.flickr.com/photos/88279756@N07/8752411274/",
 				postLinkURL: "",
 				date: new Date(Date.parse('5/1/13'))
+			},
+			{
+				postBody: "Waterfront redevelopment, down come the walls of Georgia Pacific", 
+				postVideoURL: "http://youtu.be/gXzNCosFVyk",
+				postImageURl: "",
+				postLinkURL: "",
+				date: new Date(Date.parse('5/12/13'))
 			}
 		]
 	      },
@@ -89,13 +88,6 @@ function HomeListCtrl($scope) {
 				postImageURl: "http://www.flickr.com/photos/88279756@N07/8750715483/",
 				postLinkURL: "",
 				date: new Date(Date.parse('1/12/13'))
-			},
-			{
-				postBody: "I love being SO close to the waterfront redevelopment; they're taking down Georgia Pacific piece by piece!", 
-				postVideoURL: "http://youtu.be/gXzNCosFVyk",
-				postImageURl: "",
-				postLinkURL: "",
-				date: new Date(Date.parse('3/12/13'))
 			},
 			{
 				postBody: "My neighbors rock, they're planting trees to beautify the Lettered Streets.",
